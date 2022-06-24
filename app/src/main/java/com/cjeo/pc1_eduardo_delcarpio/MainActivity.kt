1e7410f996a9ef6042b64489b51aef7f53145707
eduardo.delcarpio <eduardo.delcarpio@gmail.com>
2022-06-24 03:15:55 -0500

    pc1-eduardo-delcarpio

---
+++ app/src/main/java/com/cjeo/pc1_eduardo_delcarpio/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.cjeo.pc1_eduardo_delcarpio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
